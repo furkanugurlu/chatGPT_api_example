433a50ee1c287a085c6347ba25f06020bc209ca5
furkanugurlu <furkanugurlu3451@gmail.com>
2022-12-23 18:10:11 +0300

    about server build error

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
+
 const configutaion = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
